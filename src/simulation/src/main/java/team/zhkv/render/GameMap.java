b3c18ee62abe3876a2afa3706a1a455f6b35b40f
bonnyped <bonnyped@gmail.com>
2025-09-16 19:26:05 +0600

    modified GameMap

--- src/simulation/src/main/java/team/zhkv/render/GameMap.java
+++ src/simulation/src/main/java/team/zhkv/render/GameMap.java
@@ -1,5 +1,19 @@
 package team.zhkv.render;
 
-public class GameMap {
+import java.util.Map;
+import java.util.TreeSet;
 
+import team.zhkv.entities.Entity;
+import team.zhkv.entities.Location;
+import team.zhkv.utils.PropertiesStorage;
+
+public class GameMap {
+    private PropertiesStorage storage;
+    private Map<Location, Entity> entities;
+    private TreeSet<Location> creatures;
+    private MapRenderer renderer;
+
+    public Map<Location, Entity> getEntities() {
+        return entities;
+    }
 }
