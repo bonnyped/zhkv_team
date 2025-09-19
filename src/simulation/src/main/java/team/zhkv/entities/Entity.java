6b805bb592e94604daddd9506fda556a728969d5
bonnyped <bonnyped@gmail.com>
2025-09-19 20:57:41 +0600

    modified Entity - delete field Location

--- src/simulation/src/main/java/team/zhkv/entities/Entity.java
+++ src/simulation/src/main/java/team/zhkv/entities/Entity.java
@@ -2,18 +2,9 @@ package team.zhkv.entities;
 
 public abstract class Entity {
     int fieldQuantity;
-    Location location;
-
-    public Location getLocation() {
-        return location;
-    }
 
     public int getFieldQuantity() {
         return fieldQuantity;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
     }
 
     public void setFieldQuantity(int quantity) {
