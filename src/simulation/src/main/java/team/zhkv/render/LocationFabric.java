257bcde1ca127f683bc0ef3d5d4ad04e4391ba25
bonnyped <bonnyped@gmail.com>
2025-09-26 12:00:45 +0600

    modified LocationFabric

--- src/simulation/src/main/java/team/zhkv/render/LocationFabric.java
+++ src/simulation/src/main/java/team/zhkv/render/LocationFabric.java
@@ -1,22 +1,41 @@
 package team.zhkv.render;
 
-import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
+import java.util.Random;
 
 import team.zhkv.App;
 
 public class LocationFabric {
-    private Set<Location> locations = new HashSet<>();
     private int boardX = App.FIELD_SIZE_MIN.getDx();
     private int boardY = App.FIELD_SIZE_MIN.getDy();
+    private Set<Location> rokcs;
+
+    private Set<Location> trees;
+
+    private Set<Location> grass;
+
+    private Set<Location> herbivores;
+
+    private Set<Location> predators;
+
+    private boolean locationExists(Set<Location> s, Location l) {
+        return s != null && s.contains(l);
+    }
+
+    private boolean locationsContainsNewLocation(Location l) {
+        return locationExists(grass, l)
+                || locationExists(rokcs, l)
+                || locationExists(trees, l)
+                || locationExists(predators, l)
+                || locationExists(herbivores, l);
+    }
 
     private Location getFreeRandomLocation() {
         Random random = new Random(31);
         Location newLocation = new Location(random.nextInt(boardX),
                 random.nextInt(boardY));
 
-        while (locations.contains(newLocation)) {
+        while (locationsContainsNewLocation(newLocation)) {
             newLocation.setLocation(random.nextInt(boardX),
                     random.nextInt(boardY));
         }
@@ -26,5 +45,25 @@ public class LocationFabric {
 
     public Location buildLocation() {
         return getFreeRandomLocation();
+    }
+
+    public void setRokcs(Set<Location> rokcs) {
+        this.rokcs = rokcs;
+    }
+
+    public void setTrees(Set<Location> trees) {
+        this.trees = trees;
+    }
+
+    public void setGrass(Set<Location> grass) {
+        this.grass = grass;
+    }
+
+    public void setHerbivores(Set<Location> herbivores) {
+        this.herbivores = herbivores;
+    }
+
+    public void setPredators(Set<Location> predators) {
+        this.predators = predators;
     }
 }
