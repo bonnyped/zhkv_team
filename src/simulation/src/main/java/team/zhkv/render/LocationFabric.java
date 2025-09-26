ad031c056ce9ba0567265276d40080459dd96669
bonnyped <bonnyped@gmail.com>
2025-09-26 13:44:29 +0600

    modified LocationFabric

--- src/simulation/src/main/java/team/zhkv/render/LocationFabric.java
+++ src/simulation/src/main/java/team/zhkv/render/LocationFabric.java
@@ -1,13 +1,23 @@
 package team.zhkv.render;
 
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import team.zhkv.App;
+import team.zhkv.entities.Entity;
+import team.zhkv.entities.Grass;
+import team.zhkv.entities.Herbivore;
+import team.zhkv.entities.Predator;
+import team.zhkv.entities.Rock;
+import team.zhkv.entities.Tree;
 
 public class LocationFabric {
     private int boardX = App.FIELD_SIZE_MIN.getDx();
     private int boardY = App.FIELD_SIZE_MIN.getDy();
+    private List<Map<Location, Entity>> maps = new ArrayList<>();
     private Set<Location> rokcs;
 
     private Set<Location> trees;
@@ -17,6 +27,10 @@ public class LocationFabric {
     private Set<Location> herbivores;
 
     private Set<Location> predators;
+
+    public LocationFabric(List<Map<Location, Entity>> maps) {
+        this.maps = maps;
+    }
 
     private boolean locationExists(Set<Location> s, Location l) {
         return s != null && s.contains(l);
@@ -43,27 +57,34 @@ public class LocationFabric {
         return newLocation;
     }
 
+    public void bindSetsWithMaps(Map<Location, Entity> map,
+            Class<? extends Entity> clazz) {
+        if (clazz == Rock.class) {
+            rokcs = map.keySet();
+        } else if (clazz == Tree.class) {
+            trees = map.keySet();
+        } else if (clazz == Grass.class) {
+            grass = map.keySet();
+        } else if (clazz == Herbivore.class) {
+            herbivores = map.keySet();
+        } else if (clazz == Predator.class) {
+            predators = map.keySet();
+        }
+    }
+
     public Location buildLocation() {
         return getFreeRandomLocation();
     }
 
-    public void setRokcs(Set<Location> rokcs) {
-        this.rokcs = rokcs;
+    public Set<Location> getGrass() {
+        return grass;
     }
 
-    public void setTrees(Set<Location> trees) {
-        this.trees = trees;
+    public Set<Location> getHerbivores() {
+        return herbivores;
     }
 
-    public void setGrass(Set<Location> grass) {
-        this.grass = grass;
-    }
-
-    public void setHerbivores(Set<Location> herbivores) {
-        this.herbivores = herbivores;
-    }
-
-    public void setPredators(Set<Location> predators) {
-        this.predators = predators;
+    public Set<Location> getPredators() {
+        return predators;
     }
 }
