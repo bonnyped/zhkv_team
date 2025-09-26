7d0a188301f13245dcd7dd9be82c3bae03ee6dbf
bonnyped <bonnyped@gmail.com>
2025-09-26 13:58:18 +0600

    modified Herbivore

--- src/simulation/src/main/java/team/zhkv/actions/InitHerbivores.java
+++ src/simulation/src/main/java/team/zhkv/actions/InitHerbivores.java
@@ -1,13 +1,21 @@
 package team.zhkv.actions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import team.zhkv.entities.Entity;
+import team.zhkv.entities.Herbivore;
 import team.zhkv.render.EntitiesStorage;
+import team.zhkv.render.Location;
 
 public class InitHerbivores extends Init {
     @Override
     public void action(EntitiesStorage es) {
-        // for (int i = 0; i < gm.differenceHerbivoreCountAndMin(); i++) {
-        // createIntities(gm, new Herbivore(1));
-        // gm.incrementHerbivoreCount();
-        // }
+        Map<Location, Entity> tmp = new HashMap<>();
+        for (int i = 0; i < es.differenceEntityCountMinFact(
+                Herbivore.class); i++) {
+            tmp.put(es.getNewLocation(), new Herbivore());
+        }
+        es.getObjectsToChange().putAll(tmp);
     }
 }
