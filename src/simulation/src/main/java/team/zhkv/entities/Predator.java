349051ce1b597ee51417cef1dd2c180b7d2e66ae
bonnyped <bonnyped@gmail.com>
2025-09-30 21:12:59 +0600

    modified Predator.java

--- src/simulation/src/main/java/team/zhkv/entities/Predator.java
+++ src/simulation/src/main/java/team/zhkv/entities/Predator.java
@@ -1,5 +1,24 @@
 package team.zhkv.entities;
 
-public class Predator extends Creature {
+public class Predator extends Creature implements Damager, Eater {
     private int damage = 50;
+
+    @Override
+    public void damage(Herbivore victim) {
+        victim.hp -= damage;
+    }
+
+    @Override
+    public int getDamagePower() {
+        return damage;
+    }
+
+    @Override
+    public void eat(Eatable herbivore) {
+        if (hp < 50) {
+            hp += hp;
+        } else if (hp < 100) {
+            hp = 100;
+        }
+    }
 }
