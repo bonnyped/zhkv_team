fe55c83b64e0f52da015123333b3d6cb48e42ce4
bonnyped <bonnyped@gmail.com>
2025-09-30 21:13:25 +0600

    modified Herbivore.java

--- src/simulation/src/main/java/team/zhkv/entities/Herbivore.java
+++ src/simulation/src/main/java/team/zhkv/entities/Herbivore.java
@@ -1,4 +1,15 @@
 package team.zhkv.entities;
 
-public class Herbivore extends Creature implements Eatable {
+public class Herbivore extends Creature implements Eatable, Eater, Damageble {
+    @Override
+    public void eat(Eatable entity) {
+        if (hp < 100) {
+            hp += hp;
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        hp -= damage;
+    }
 }
