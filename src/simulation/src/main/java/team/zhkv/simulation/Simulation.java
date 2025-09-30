adea3a03d9954604dcfb8cd7e638b71bfd05b54a
bonnyped <bonnyped@gmail.com>
2025-09-30 21:17:11 +0600

    modified Simulation

--- src/simulation/src/main/java/team/zhkv/simulation/Simulation.java
+++ src/simulation/src/main/java/team/zhkv/simulation/Simulation.java
@@ -9,47 +9,50 @@ import team.zhkv.actions.TurnGrass;
 import team.zhkv.actions.TurnHerbivores;
 import team.zhkv.actions.Turn;
 import team.zhkv.actions.TurnMove;
+import team.zhkv.render.FinalRenderer;
 import team.zhkv.render.GameMap;
-import team.zhkv.render.FullEmojiRenderer;
+import team.zhkv.render.IntermediateRenderer;
+import team.zhkv.render.Renderable;
 
 public class Simulation {
-    private GameMap gameMap = new GameMap();
-    private FullEmojiRenderer renderer = new FullEmojiRenderer();
-    private List<Action> initActions = List.of(
-            new InitAllEntities());
-    private List<Action> turnActions = List.of(
-            new TurnGrass(),
-            new TurnHerbivores(),
-            new TurnMove()
-    // здесь нужно дёргать рендерер и печатать поле
-    // вместе с цветом атаки и направлением движения
-    // затем нужно будет дернуть удаление старых локаций
-    // и внесение новых локаций в общий список
-    // new TurnRender()
-    );
+        private GameMap gameMap = new GameMap();
+        private List<Action> initActions = List.of(
+                        new InitAllEntities());
+        private List<Action> turnActions = List.of(
+                        new TurnGrass(),
+                        new TurnHerbivores(),
+                        new TurnMove()
+        // здесь нужно дёргать рендерер и печатать поле
+        // вместе с цветом атаки и направлением движения
+        // затем нужно будет дернуть удаление старых локаций
+        // и внесение новых локаций в общий список
+        // new TurnRender()
+        );
+        private Renderable renderer;
 
-    private int stepsCount = 0;
+        private int stepsCount = 0;
 
-    private void nextTurn() {
-        turnActions.stream()
-                .map(Turn.class::cast)
-                .forEach(turn -> turn.action(gameMap));
-    }
-
-    public void startSimulation() {
-        initActions.stream()
-                .map(Init.class::cast)
-                .forEach(init -> init.action(gameMap));
-        while (stepsCount != 21) {
-            renderer.render(gameMap.getWholeMapEntities());
-            ++stepsCount;
-            nextTurn();
-            renderer.render(gameMap.getWholeMapEntities(), gm.getAllStepChanges());
+        private void nextTurn() {
+                turnActions.stream()
+                                .map(Turn.class::cast)
+                                .forEach(turn -> turn.action(gameMap));
         }
-    }
 
-    public void pauseSimulation() {
+        public void startSimulation() {
+                initActions.stream()
+                                .map(Init.class::cast)
+                                .forEach(init -> init.action(gameMap));
+                while (stepsCount != 21) {
+                        new FinalRenderer().render(gameMap);
+                        ++stepsCount;
+                        nextTurn();
+                        new IntermediateRenderer().render(gameMap);
+                        gameMap.applyChanges();
+                }
+        }
 
-    }
+        public void pauseSimulation() {
+
+        }
 
 }
