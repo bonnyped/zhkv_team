d0fe3e998bdc05d2eed82beedc46726a25735bca
bonnyped <bonnyped@gmail.com>
2025-10-07 20:17:50 +0600

    modified Creature.java

--- src/simulation/src/main/java/team/zhkv/entities/Creature.java
+++ src/simulation/src/main/java/team/zhkv/entities/Creature.java
@@ -1,7 +1,7 @@
 package team.zhkv.entities;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 import team.zhkv.move.Location;
 import team.zhkv.render.GameMap;
@@ -16,10 +16,10 @@ public abstract class Creature extends Entity implements Moveble, Eater {
 
     @Override
     public Location makeMove(GameMap gm, Location oldLocation) {
-        List<Location> path = new BFS().build(gm, oldLocation)
+        List<Location> path = new BFS().build(gm, oldLocation, food)
                 .getPath();
 
-        return path.isEmpty() ? null : path.get(speed - 1);
+        return path.isEmpty() ? null : applySpeedToPath(gm, path);
     }
 
     public Class<? extends Entity> getFood() {
@@ -36,5 +36,28 @@ public abstract class Creature extends Entity implements Moveble, Eater {
 
     public int getSpeed() {
         return speed;
+    }
+
+    private Location applySpeedToPath(GameMap gm, List<Location> path) {
+        if (isSpeedOrPathSizeEqualsOneCell(path.size())) {
+            return path.get(0);
+        } else if (isSpeedGreaterThenOneCell()
+                && isTargetFood(gm.getEntity(path.get(speed - 1)))) {
+            return path.get(speed - 2);
+        } else {
+            return path.get(speed - 1);
+        }
+    }
+
+    private boolean isSpeedOrPathSizeEqualsOneCell(int size) {
+        return speed == 1 || size == 1;
+    }
+
+    private boolean isSpeedGreaterThenOneCell() {
+        return speed > 1;
+    }
+
+    private boolean isTargetFood(Entity target) {
+        return target != null && target.getClass() == food;
     }
 }
