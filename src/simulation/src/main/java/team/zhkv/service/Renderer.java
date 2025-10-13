d2a47201064f3942187533b0981ad04d39ad665d
bonnyped <bonnyped@gmail.com>
2025-10-13 10:46:31 +0600

    minor changes

--- src/simulation/src/main/java/team/zhkv/service/Renderer.java
+++ src/simulation/src/main/java/team/zhkv/service/Renderer.java
@@ -72,8 +72,8 @@ public class Renderer implements Renderable {
     private void printMenu() {
         System.out.println("\n\n\n=========================== МЕНЮ ===========================");
         System.out.println();
-        System.out.println("                1. p — пауза/возобновление");
-        System.out.println("             2. cntrl + с — выход из программы");
+        System.out.println("                1. p — pause/resume");
+        System.out.println("             2. cntrl + с — quit program");
         System.out.println();
     }
 }
